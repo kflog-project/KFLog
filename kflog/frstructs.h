cd6f2809b4e9aa15b5444e84e0b5fb7d56fbf612
heiner <heiner@60db0586-fb42-0410-ab30-5566f8710cf8>
2002-04-10 19:36:11 +0000

    Added missing frstructs.h

---
+++ kflog/frstructs.h
@@ -0,0 +1,63 @@
+/***********************************************************************
+**
+**   struct.h
+**
+**   This file is part of kio-logger.
+**
+************************************************************************
+**
+**   Copyright (c):  2002 by Heiner Lamprecht
+**
+**   This file is distributed under the terms of the General Public
+**   Licence. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#include <time.h>
+#include <qstring.h>
+
+/**
+ *
+ */
+struct FRDirEntry
+{
+  /**
+   * Contains the name of the pilot. If there is a co-pilot in the
+   * glider, he is not listed here.
+   */
+  QString pilotName;
+  /**
+   * The ID of the glider
+   */
+  QString gliderID;
+  /**
+   * The type of the glider.
+   */
+  QString gliderType;
+  /**
+   * The short filename used for this flight. The naming sheme is
+   * defined by the IGC.
+   */
+  QString shortFileName;
+  /**
+   * The long filename used for this flight. The naming sheme is
+   * defined by the IGC.
+   */
+  QString longFileName;
+  /**
+   * The time of the first fix (i.e. first B-record) of the flight.
+   * According to the IGC-specification, the time is given in UTC.
+   */
+  tm firstTime;
+  /**
+   * The time of the first fix (i.e. first B-record) of the flight.
+   * According to the IGC-specification, the time is given in UTC.
+   */
+  tm lastTime;
+  /**
+   * The duration of the flight in seconds.
+   */
+  int duration;
+};
