ea637f3fa0de466f7df490727118933384dcf952
heiner <heiner@60db0586-fb42-0410-ab30-5566f8710cf8>
2003-07-16 19:42:36 +0000

    Re-Adding OpenGL-files.

---
+++ kflog/opengl_igc/glview.cpp
@@ -0,0 +1,380 @@
+/***********************************************************************
+**
+**   glview.cpp
+**
+**   This file is part of KFLog2.
+**
+************************************************************************
+**
+**   Copyright (c):  2003 by Christof Bodner
+**
+**   This file is distributed under the terms of the General Public
+**   Licence. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+/***********************************************************************
+**
+**  Attention: y values have to be passed negative to OpenGL functions!
+**
+***********************************************************************/
+
+
+#include "glview.h"
+
+#include <math.h>
+#include <stdlib.h>
+#include <kaction.h>
+#include <klocale.h>
+#include "../flight.h"
+#include "../resource.h"
+#include "../mapcontents.h"
+
+
+
+/*!
+  This is the main class for the OpenGL implementation. The constructor
+  initializes some variables.
+*/
+
+GLView::GLView( QWidget* parent, const char* name )
+    : QGLWidget( parent, name )
+{
+    // These values should be stored KConfig
+    xRot = -45.0;
+    yRot = 0.0;
+    zRot = -30.0;		        // default object rotation
+    scale = 1.25;			      // default object scale
+    heightExaggerate=2.0;   //  exaggerates the heights
+
+    // Initializations
+    deltaX = 0.0;           // initial position of objects
+    deltaY = 0.0;
+    deltaZ = 0.0;
+    boxObject = 0;          // bounding box object (0 means none)
+    flightList.clear();     // no flights in list
+}
+
+
+/*!
+  Release allocated resources
+*/
+
+GLView::~GLView()
+{
+    if (boxObject)
+      glDeleteLists( boxObject, 1 );
+    QValueList<GLuint>::iterator it;
+    for ( it = flightList.begin(); it != flightList.end(); ++it )
+      glDeleteLists((*it),1);
+}
+
+void GLView::addShadow(Flight* flight)
+{
+    GLuint list;
+    unsigned int length;
+
+    if (flight){
+      length=flight->getRouteLength();
+      extern MapMatrix _globalMapMatrix;
+      QPoint point;
+      list = glGenLists( 1 );
+
+      glNewList( list, GL_COMPILE );
+
+      qglColor( gray );		      // Shorthand for glColor3f or glIndex
+      glLineWidth( 2.0 );
+
+      flightPoint fPoint;
+      int actx, acty, actz;
+      fPoint=flight->getPoint(0);
+      actx=fPoint.projP.x();
+      acty=-fPoint.projP.y();  // Attention!
+      actz=minz;
+      glVertex3i(  actx,  acty, actz );
+      minx=actx; maxx=actx; miny=acty; maxy=acty; minz=actz; maxz=actz;
+
+      glBegin( GL_LINE_LOOP );
+      for (int i=1;i<length;i++){
+        fPoint=flight->getPoint(i);
+        actx=fPoint.projP.x();
+        acty=-fPoint.projP.y();
+        glVertex3i(  actx,  acty, actz );
+      }
+      glEnd();
+      glEndList();
+
+      flightList.append(list);
+    }
+}
+
+void GLView::addFlight(Flight* flight)
+{
+    GLuint list;
+    unsigned int length;
+
+    if (flight){
+      length=flight->getRouteLength();
+      qWarning(QString("Adding %1 points").arg(length));
+
+      extern MapMatrix _globalMapMatrix;
+      extern MapConfig _globalMapConfig;
+      
+      QPoint point;
+        
+      list = glGenLists( 1 );
+
+      glNewList( list, GL_COMPILE );
+
+      qglColor( blue );		      // Shorthand for glColor3f or glIndex
+
+      glLineWidth( 2.0 );
+
+      
+      flightPoint fPoint;
+      int actx, acty, actz;
+      fPoint=flight->getPoint(0);
+      actx=fPoint.projP.x();
+      acty=-fPoint.projP.y();  // Attention!
+      actz=fPoint.height;
+      glVertex3i(  actx,  acty, actz );
+      minx=actx; maxx=actx; miny=acty; maxy=acty; minz=actz; maxz=actz;
+
+      glBegin( GL_LINE_LOOP );
+      for (int i=1;i<length;i++){
+        fPoint=flight->getPoint(i);
+        actx=fPoint.projP.x();
+        acty=-fPoint.projP.y();
+        actz=fPoint.height;
+        
+        QPen drawP = _globalMapConfig.getDrawPen(&fPoint,true);
+        qglColor(drawP.color());
+        glVertex3i(  actx,  acty, actz );
+        maxx=MAX(actx,maxx);
+        minx=MIN(actx,minx);
+        maxy=MAX(acty,maxy);
+        miny=MIN(acty,miny);
+        maxz=MAX(actz,maxz);
+        minz=MIN(actz,minz);
+      }
+      glEnd();
+      glEndList();
+
+      deltaX=-(maxx+minx)/2.0;
+      deltaY=-(maxy+miny)/2.0;
+      deltaZ=-(maxz+minz)/2.0;
+      flightList.append(list);
+      // change Bounding Box
+      scale=MAX(abs(maxx-minx),abs(maxy-miny));
+      scale=MAX(scale,abs(maxz-minz));
+      scale=1.0/scale;
+      if (boxObject)
+        glDeleteLists( boxObject, 1 );
+      boxObject = makeBoxObject();
+      addShadow(flight);
+    }
+}
+
+/*!
+  The actual openGL commands for drawing are performed here.
+  This function is called on every paint event.
+*/
+
+void GLView::paintGL()
+{
+//    qWarning("GLBox::paintGL()");
+    // initialise the state machine (here will be more e.g. the lighting)
+    glClear( GL_COLOR_BUFFER_BIT );
+    glLoadIdentity();  // unity matrix
+
+    // the following lines are for the transformations
+    // You have to read them "bottom up". E.g. the last translation
+    // is actually the first that is performed (This is due to the
+    // matrix operation)
+
+    // move a little bit away from camera
+    glTranslatef( 0.0, 0.0, -10.0 );
+
+    // rotate around x-, y- and z-axis
+    glRotatef( xRot, 1.0, 0.0, 0.0 ); 
+    glRotatef( yRot, 0.0, 1.0, 0.0 ); 
+    glRotatef( zRot, 0.0, 0.0, 1.0 );
+
+    // scale objects
+    glScalef( scale, scale, scale*heightExaggerate );
+    // move the center of the object to 0,0,0
+    glTranslatef( deltaX, deltaY, deltaZ );
+
+    // now all objects that should be displayed
+    // bounding box
+    glCallList( boxObject );
+    // flight list (including the shadows of the flights)
+    QValueList<GLuint>::iterator it;
+    for ( it = flightList.begin(); it != flightList.end(); ++it )
+      glCallList((*it));
+}
+
+
+/*!
+  Set up the OpenGL rendering state, and define display list. Called once on the
+  initialization of the widget
+*/
+
+void GLView::initializeGL()
+{
+    qWarning("GLBox::initializeGL()");
+//    qglClearColor( black ); 		// Let OpenGL clear to black
+    qglClearColor(getBackgroundColor());
+    glShadeModel( GL_FLAT );    // shading model
+}
+
+
+
+/*!
+  Set up the OpenGL view port, matrix mode, etc. Here is the projection mode
+*/
+
+void GLView::resizeGL( int w, int h )
+{
+    qWarning("GLBox::resizeGL()");
+    glViewport( 0, 0, (GLint)w, (GLint)h );
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glFrustum( -1.0, 1.0, -1.0*h/w, 1.0*h/w, 5.0, 16.0 );
+    glMatrixMode( GL_MODELVIEW );
+}
+
+
+/*!
+  Generate an OpenGL display list for the object to be shown, i.e. the bounding box
+*/
+
+GLuint GLView::makeBoxObject()
+{	
+    GLuint list;                // OpenGL objects are referred to as unsigned integers
+
+    list = glGenLists( 1 );     // generate new list (empty)
+
+    glNewList( list, GL_COMPILE );  // set mode = compile (=faster in rendering, but
+                                    // no changes allowed
+
+    qglColor( white );		      // Shorthand for glColor3f or glIndex
+
+    glLineWidth( 2.0 );
+
+    glBegin( GL_LINE_LOOP );    //  Begin new line segment (closed)
+    glVertex3f(  maxx,  maxy,  minz );   // define verteces as floating öoint values)
+    glVertex3f(  maxx,  miny,  minz );
+    glVertex3f(  minx,  miny,  minz );
+    glVertex3f(  minx,  maxy,  minz );
+    glEnd();                    //  End of segment
+
+    glBegin( GL_LINE_LOOP );    // same as above
+    glVertex3f(  maxx,  maxy, maxz );
+    glVertex3f(  maxx,  miny, maxz );
+    glVertex3f(  minx,  miny, maxz );
+    glVertex3f(  minx,  maxy, maxz );
+    glEnd();
+
+    glBegin( GL_LINES );         // these are 4 lines (not closed, so 8 points)
+    glVertex3f(  maxx,  maxy,  minz );   glVertex3f(  maxx,  maxy, maxz );
+    glVertex3f(  maxx,  miny,  minz );   glVertex3f(  maxx,  miny, maxz );
+    glVertex3f(  minx,  miny,  minz );   glVertex3f(  minx,  miny, maxz );
+    glVertex3f(  minx,  maxy,  minz );   glVertex3f(  minx,  maxy, maxz );
+    glEnd();
+
+    glEndList();                 // end of object
+
+    return list;                  // return object identifier
+}
+
+/*!
+  Set the rotation angle of the object to \e degrees around the X axis.
+*/
+
+void GLView::setXRotation( int degrees )
+{
+    xRot = (GLfloat)(degrees % 360);
+    updateGL();
+}
+
+
+/*!
+  Set the rotation angle of the object to \e degrees around the Y axis.
+*/
+
+void GLView::setYRotation( int degrees )
+{
+    yRot = (GLfloat)(degrees % 360);
+    updateGL();
+}
+
+/*!
+  Set the rotation angle of the object to \e degrees around the Z axis.
+*/
+
+void GLView::setZRotation( int degrees )
+{
+    zRot = (GLfloat)(degrees % 360);
+    updateGL();
+}
+
+/*!
+  Zooms scene
+*/
+
+void GLView::zoom( float scalefactor )
+{
+    scale*=scalefactor;
+    updateGL();
+}
+
+/*
+  Slots
+*/
+void GLView::mousePressEvent ( QMouseEvent * e )
+{
+  mouse_last=e->pos();  // store mouse position
+}
+
+void GLView::mouseMoveEvent ( QMouseEvent * e )
+{
+  if (e->state() & LeftButton){  // left button means translation
+    float phi=zRot/180.0*M_PI;
+//    qWarning(QString("phi:%1").arg(phi));
+    float dx=(mouse_last.x()-e->x())/100.0/scale;
+    float dy=(mouse_last.y()-e->y())/100.0/scale;
+    if (((int)xRot % 360)>-90)
+      dy=-dy;
+    deltaX-=dx*cos(phi)+dy*sin(phi);
+    deltaY+=dx*sin(phi)-dy*cos(phi);
+    mouse_last=e->pos();
+    updateGL();
+  }
+  else if(e->state() & RightButton){  // right button is rotation
+    zRot-=mouse_last.x()-e->x();
+    xRot-=mouse_last.y()-e->y();
+    mouse_last=e->pos();
+    updateGL();
+//    qWarning(QString("zRot:%1 xRot:%2").arg(zRot).arg(xRot));
+  }
+}
+
+/*!
+  Zoom objects with mouse wheel
+*/
+
+void GLView::wheelEvent ( QWheelEvent * e )
+{
+  float factor=e->delta()/100.0;
+  if (factor<0)
+    factor=-1/factor;
+  zoom(factor);
+}
+
+QColor GLView::getBackgroundColor()
+{
+    return QColor(64,102,128);
+}
---
+++ kflog/opengl_igc/openglwidget.cpp
@@ -0,0 +1,50 @@
+/***********************************************************************
+**
+**   openglwidget.cpp
+**
+**   This file is part of KFLog2.
+**
+************************************************************************
+**
+**   Copyright (c):  2003 by Christof Bodner
+**
+**   This file is distributed under the terms of the General Public
+**   Licence. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
+#include <qlayout.h>
+#include <kmessagebox.h>
+#include <klocale.h>
+#include <qlabel.h>
+#include "openglwidget.h"
+#include "glview.h"
+
+OpenGLWidget::OpenGLWidget(QWidget* parent, char *name)
+  :QWidget(parent,name){
+   glview=new GLView(this,"OpenGL Viewer");
+   QHBoxLayout* flayout = new QHBoxLayout( this, 2, 2, "flayout");
+
+   if (!glview->isValid())
+   {
+     QString text=i18n("No OpenGL extension for display found! Check your configuration!");
+     KMessageBox::error(0, "<qt>" + text +"</qt>");
+     QLabel* label= new QLabel(text,this);
+     label->setAlignment( AlignHCenter|AlignVCenter );
+     flayout->addWidget(label,1);
+   }
+   else
+   {
+     flayout->addWidget( glview, 1 );
+   }
+}
+
+OpenGLWidget::~OpenGLWidget(){
+}
+
+void OpenGLWidget::addFlight(Flight* flight){
+  if (glview->isValid())
+    glview->addFlight(flight);
+}
