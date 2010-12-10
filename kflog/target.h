92a17bc4f54e22db3408b84d1648092824b10b45
axel <axel@60db0586-fb42-0410-ab30-5566f8710cf8>
2010-12-10 21:25:59 +0000

    target file containing current version introduced

---
+++ kflog/target.h
@@ -0,0 +1,30 @@
+/***************************************************************************
+        target.h  -  Defines KFLog release version for the build target.
+	             This file is also used during Debian package build
+                     as release information.
+
+                             -------------------
+    begin                : 2010-12-10
+    copyright            : (C) 2010 by Axel Pauli
+
+    Email of maintainer  : hendrik@kflog.org
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   $Id$
+ *                                                                         *
+***************************************************************************/
+
+#ifndef target_h
+#define target_h
+
+#define KFLOG_VERSION "4.0.0"
+
+#endif
