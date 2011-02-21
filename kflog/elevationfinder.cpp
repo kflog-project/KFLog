73006248888dabf34816a28510a45fbbd5321a1c
axel <axel@60db0586-fb42-0410-ab30-5566f8710cf8>
2011-02-21 20:56:42 +0000

    fully portage to Qt4

--- kflog/elevationfinder.cpp
+++ kflog/elevationfinder.cpp
@@ -1,27 +1,35 @@
-//
-// C++ Implementation: elevationfinder
-//
-// Description:
-//
-//
-// Author: Andr� Somers <andre@kflog.org>, (C) 2004
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
-#include "elevationfinder.h"
-#include <qapplication.h>
-#include <q3textstream.h>
-#include <qdir.h>
-#include <qrect.h>
-#include <mapcontents.h>
-#include <mapmatrix.h>
+/***********************************************************************
+**
+**   elevationfinder.h
+**
+**   This file is part of KFLog4.
+**
+************************************************************************
+**
+**   Copyright (c): 2004 by André Somers <andre@kflog.org>
+**                  2011 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
 
-ElevationFinder * ElevationFinder::_instance = 0;
+#include <QtGui>
+
+#include "elevationfinder.h"
+#include "mapcontents.h"
+#include "mapmatrix.h"
+
+extern MapContents *_globalMapContents;
+extern MapMatrix *_globalMapMatrix;
 
 ElevationFinder::ElevationFinder(QObject *parent) :
  QObject(parent),
- useOGIE(false)
+ useOGIE(false),
+ demFile(0),
+ demStream(0)
 {
   setObjectName("ElevationFinder");
 
@@ -31,47 +39,40 @@ ElevationFinder::ElevationFinder(QObject *parent) :
     }
 
   timer = new QTimer(this);
+  timer-> setSingleShot( true );
   connect(timer, SIGNAL(timeout()), this, SLOT(timeout()));
-
-  demFile = 0;
-  demStream = 0;
 }
 
 
 ElevationFinder::~ElevationFinder()
 {
-  if (timer)
-    delete timer;
-
   timeout(); //takes care of deleting demStream and demFile
 }
 
-ElevationFinder * ElevationFinder::instance()
+ElevationFinder* ElevationFinder::instance()
 {
-  if (! _instance )
-    _instance = new ElevationFinder(qApp);
-  return _instance;
+  static ElevationFinder instance(qApp);
+
+  return &instance;
 }
 
 int ElevationFinder::elevationWgs(const QPoint& coordinates)
 {
-  extern MapContents *_globalMapContents;
-  extern MapMatrix *_globalMapMatrix;
-
-  if (useOGIE) //use openGLIGCexplorer's DEM file
-    return findDEMelevation(coordinates);
+  if( useOGIE ) //use openGLIGCexplorer's DEM file
+    {
+      return findDEMelevation( coordinates );
+    }
 
   //use the 'old' method
-  return _globalMapContents->getElevation( _globalMapMatrix->wgsToMap(coordinates), 0 );
+  return _globalMapContents->getElevation( _globalMapMatrix->wgsToMap( coordinates ), 0 );
 }
 
 int ElevationFinder::elevationMap(const QPoint& coordinates)
 {
-  extern MapContents *_globalMapContents;
-  extern MapMatrix *_globalMapMatrix;
-
-  if (useOGIE) //use openGLIGCexplorer's DEM file
-    return findDEMelevation(_globalMapMatrix->mapToWgs(coordinates));
+  if( useOGIE ) //use openGLIGCexplorer's DEM file
+    {
+      return findDEMelevation( _globalMapMatrix->mapToWgs( coordinates ) );
+    }
 
   //use the 'old' method
   return _globalMapContents->getElevation( coordinates, 0 );
@@ -79,57 +80,64 @@ int ElevationFinder::elevationMap(const QPoint& coordinates)
 
 int ElevationFinder::elevation(const QPoint& WgsCoordinates, const QPoint& MapCoordinates)
 {
-  extern MapContents *_globalMapContents;
-
-  if (useOGIE) //use openGLIGCexplorer's DEM file
-    return findDEMelevation(WgsCoordinates);
+  if( useOGIE ) //use openGLIGCexplorer's DEM file
+    {
+      return findDEMelevation( WgsCoordinates );
+    }
 
   //use the 'old' method
-  return _globalMapContents->getElevation(MapCoordinates, 0);
+  return _globalMapContents->getElevation( MapCoordinates, 0 );
 }
 
 int ElevationFinder::findDEMelevation(const QPoint& coordinates)
 {
   timer->stop();
 
-  if (!demFile) {
-    demFile = new QFile(demFileName);
-    demFile->open(QIODevice::ReadOnly);
-    demStream = new QDataStream(demFile);
-  }
+  if( !demFile )
+    {
+      demFile = new QFile( demFileName );
+      demFile->open( QIODevice::ReadOnly );
+      demStream = new QDataStream( demFile );
+    }
 
   QRect r(QPoint(demBR.x(), demTL.y()), QPoint(demTL.x(), demBR.y()));
-  if ( !r.contains(coordinates.x(), coordinates.y()) ) {
-    //qDebug("Requested point outside DEM coverage.");
-    return -1;
-  }
-  int row = (demTL.x() - coordinates.x()) / demGridLat;
-  //qDebug("row: %d", row);
-  int col = -1 * (demTL.y() - coordinates.y()) / demGridLon;
-  //qDebug("col: %d, demCols: %d", col, demCols);
-  Q_INT64 cell = 2 * (row * demCols + col);
-  //qDebug("cell: %d", cell);
-  Q_INT16 h;
 
-  demFile->at(cell);
-  *demStream>>h;
-  if (h == -9999)
-    h=0;
+  if( !r.contains( coordinates.x(), coordinates.y() ) )
+    {
+      //qDebug("Requested point outside DEM coverage.");
+      return -1;
+    }
+
+  int row = (demTL.x() - coordinates.x()) / demGridLat;
+  int col = -1 * (demTL.y() - coordinates.y()) / demGridLon;
+  qint64 cell = 2 * (row * demCols + col);
+  qint16 h;
+
+  demFile->seek( cell );
+  *demStream >> h;
+
+  if( h == -9999 )
+    {
+      h = 0;
+    }
 
   return h;
 
-  timer->start(10000, true);
+  timer->start( 10000 );
 }
 
 void ElevationFinder::timeout()
 {
-  if (demStream)
-    delete demStream;
+  if( demStream )
+    {
+      delete demStream;
+    }
 
-  if (demFile) {
-    demFile->close();
-    delete demFile;
-  }
+  if( demFile )
+    {
+      demFile->close();
+      delete demFile;
+    }
 
   demStream = 0;
   demFile = 0;
@@ -150,79 +158,116 @@ bool ElevationFinder::tryOpenGLIGCexplorer()
   int dem_grid_lon = 256;
 
 
-  //can we find openGLIGCexplorer's configuration file?
-  QFile f(QDir::home().path() + "/.openGLIGCexplorerrc");
-  if ( !f.exists() )
-    return false;
+  // can we find openGLIGCexplorer's configuration file?
+  QFile file( QDir::home().path() + "/.openGLIGCexplorerrc" );
 
-  qDebug("config file found.");
+  if( !file.exists() )
+    {
+      return false;
+    }
 
-  if (! f.open(QIODevice::ReadOnly)) {
-    qDebug("error opening file.");
-    return false;
-  }
+  if( !file.open( QIODevice::ReadOnly ) )
+    {
+      qWarning() << "Cannot open file:" << file.fileName();
+      return false;
+    }
 
-  Q3TextStream s(&f);
+  QTextStream s(&file);
+
   //let's try to find the DEM file in use from the configuration file
-  while (true) {
-    line = s.readLine();
-    if ( line.isNull() )
-      break;
-    if ( line.isEmpty() )
-      continue;
-    //qDebug("line: %s",line.toLatin1().data());
+  while( ! s.atEnd() )
+    {
+      line = s.readLine();
 
-    if ( line.startsWith("DEM_FILE") ) {
-      flags |= dem_name;
-      demFileName = line.mid(9).stripWhiteSpace();
-    }
-    if ( line.startsWith("DEM_ROWS") ) {
-      flags |= dem_rows;
-      demRows = line.mid(9).stripWhiteSpace().toInt();
-    }
-    if ( line.startsWith("DEM_COLUMNS") ) {
-      flags |= dem_cols;
-      demCols = line.mid(12).stripWhiteSpace().toInt();
-    }
-    if ( line.startsWith("DEM_LAT_MIN") ) {
-      flags |= dem_top;
-      demBR.setX( (int)line.mid(12).stripWhiteSpace().toDouble() * 600000);
-    }
-    if ( line.startsWith("DEM_LAT_MAX") ) {
-      flags |= dem_bottom;
-      demTL.setX( (int)line.mid(12).stripWhiteSpace().toDouble() * 600000);
-    }
-    if ( line.startsWith("DEM_LON_MIN") ) {
-      flags |= dem_left;
-      demTL.setY( (int)line.mid(12).stripWhiteSpace().toDouble() * 600000 );
-    }
-    if ( line.startsWith("DEM_LON_MAX") ) {
-      flags |= dem_right;
-      demBR.setY( (int)line.mid(12).stripWhiteSpace().toDouble() * 600000 );
-    }
-    if ( line.startsWith("DEM_GRID_LAT") ) {
-      flags |= dem_grid_lat;
-      demGridLat = int(line.mid(13).stripWhiteSpace().toDouble() * 600000.0);
-      qDebug("%s -> %d", line.mid(13).stripWhiteSpace().toLatin1().data(), demGridLat);
-    }
-    if ( line.startsWith("DEM_GRID_LON") ) {
-      flags |= dem_grid_lon;
-      demGridLon = int(line.mid(13).stripWhiteSpace().toDouble() * 600000.0);
-      qDebug("%s -> %d", line.mid(13).stripWhiteSpace().toLatin1().data(), demGridLat);
+      if( line.isEmpty() )
+        {
+          continue;
+        }
+      //qDebug("line: %s",line.toLatin1().data());
+
+      if( line.startsWith( "DEM_FILE" ) )
+        {
+          flags |= dem_name;
+          demFileName = line.mid( 9 ).trimmed();
+        }
+
+      else if( line.startsWith( "DEM_ROWS" ) )
+        {
+          flags |= dem_rows;
+          demRows = line.mid( 9 ).trimmed().toInt();
+        }
+
+      else if( line.startsWith( "DEM_COLUMNS" ) )
+        {
+          flags |= dem_cols;
+          demCols = line.mid( 12 ).trimmed().toInt();
+        }
+
+      else if( line.startsWith( "DEM_LAT_MIN" ) )
+        {
+          flags |= dem_top;
+          demBR.setX( (int) line.mid( 12 ).trimmed().toDouble()
+              * 600000 );
+        }
+
+      else if( line.startsWith( "DEM_LAT_MAX" ) )
+        {
+          flags |= dem_bottom;
+          demTL.setX( (int) line.mid( 12 ).trimmed().toDouble()
+              * 600000 );
+        }
+
+      else if( line.startsWith( "DEM_LON_MIN" ) )
+        {
+          flags |= dem_left;
+          demTL.setY( (int) line.mid( 12 ).trimmed().toDouble()
+              * 600000 );
+        }
+
+      else if( line.startsWith( "DEM_LON_MAX" ) )
+        {
+          flags |= dem_right;
+          demBR.setY( (int) line.mid( 12 ).trimmed().toDouble()
+              * 600000 );
+        }
+
+      else if( line.startsWith( "DEM_GRID_LAT" ) )
+        {
+          flags |= dem_grid_lat;
+          demGridLat = int( line.mid( 13 ).trimmed().toDouble()
+              * 600000.0 );
+          qDebug( "%s -> %d",
+                  line.mid( 13 ).trimmed().toLatin1().data(), demGridLat );
+        }
+
+      else if( line.startsWith( "DEM_GRID_LON" ) )
+        {
+          flags |= dem_grid_lon;
+          demGridLon = int( line.mid( 13 ).trimmed().toDouble()
+              * 600000.0 );
+          qDebug( "%s -> %d",
+                  line.mid( 13 ).trimmed().toLatin1().data(), demGridLat );
+        }
+
+      if( flags == 511 ) //all needed fields are found
+        {
+          break;
+        }
     }
 
-    if (flags == 511) //all needed fields are found
-      break;
-  }
+  file.close();
+
   //qDebug("flags: %d", flags);
 
   if (flags != 511) //we did not find all the fields we need
-    return false;
-  qDebug("demTL.x(): %d, demTL.y() %d", demTL.x(), demTL.y());
-  qDebug("demBR.x(): %d, demBR.y() %d", demBR.x(), demBR.y());
+    {
+      return false;
+    }
 
-  qDebug("good: found DEM file using openGLIGCexplorer's configuration file!");
+  // qDebug("demTL.x(): %d, demTL.y() %d", demTL.x(), demTL.y());
+  // qDebug("demBR.x(): %d, demBR.y() %d", demBR.x(), demBR.y());
+
+  qDebug("Good! Found DEM file using openGLIGCexplorer's configuration file!");
+
   return true;
 }
-
-//#include "elevationfinder.moc"
--- kflog/elevationfinder.h
+++ kflog/elevationfinder.h
@@ -1,97 +1,117 @@
-//
-// C++ Interface: elevationfinder
-//
-// Description: 
-//
-//
-// Author: Andr� Somers <andre@kflog.org>, (C) 2004
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***********************************************************************
+**
+**   elevationfinder.h
+**
+**   This file is part of KFLog4.
+**
+************************************************************************
+**
+**   Copyright (c): 2004 by André Somers <andre@kflog.org>
+**                  2011 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+**   $Id$
+**
+***********************************************************************/
+
 #ifndef ELEVATION_FINDER_H
 #define ELEVATION_FINDER_H
 
-#include <qobject.h>
-#include <qpoint.h>
-#include <qtimer.h>
-#include <qfile.h>
-#include <qdatastream.h>
+#include <QObject>
+#include <QPoint>
+#include <QTimer>
+#include <QFile>
+#include <QDataStream>
 
 /**
- * @short Finds the ground elevation for a given coordinate. Singleton.
+ * \class ElevationFinder
+ *
+ * \author André Somers, Axel Pauli
+ *
+ * \short Finds the ground elevation for a given coordinate.
+ *
  * This singleton class can use multiple (well, currently two) methods
  * to find the elevation for a given coordinate. 
  * 1. Using a DEM file configured for openGLIGCexplorer
  * 2. Using the internal isohypses
  * The preferred method is number 1, as it is much faster and more detailed
  * in comparison to the latter. 
- * @author Andr� Somers
+ *
+ * \date 2004-2011
+ *
+ * \version $Id$
  */
 class ElevationFinder : public QObject
 {
- Q_OBJECT
+  Q_OBJECT
+
+ private:
+
+  Q_DISABLE_COPY ( ElevationFinder )
+
+  ElevationFinder(QObject *parent = 0);
 
  public:
 
-    ElevationFinder(QObject *parent = 0);
+  ~ElevationFinder();
+  /**
+   * Find an elevation.
+   * @returns Elevation in meters or -1 if no valid result was found.
+   * @args coordinates Coordinate of the point in wgs format, that is, unprojected.
+   */
+  int elevationWgs(const QPoint& coordinates);
+  /**
+   * Find an elevation.
+   * @returns Elevation in meters or -1 if no valid result was found.
+   * @args coordinates Coordinate of the point in map projection format.
+   */
+  int elevationMap(const QPoint& coordinates);
+  /**
+   * Find an elevation.
+   * @returns Elevation in meters or -1 if no valid result was found.
+   * @args WgsCoordinates Coordinate of the point in wgs format, that is, unprojected.
+   * @args MapCoordinates Coordinate of the point in map projection format.
+   * Both arguments should represent the same point. The format that results in the
+   * fastest lookup for the method in use will be used. This is the preferred
+   * method of getting the elevation.
+   */
+  int elevation(const QPoint& WgsCoordinates, const QPoint& MapCoordinates);
+  /**
+   * @returns A pointer to the instance of the object to use. Use only this static
+   * method to get an ElevationFinder object!
+   */
+  static ElevationFinder *instance();
+  /**
+   * @returns true if KFLog we rely on the isohypses to find the elevation for a position.
+   */
+  bool useIsohypseForElevation() {return !useOGIE;};
 
-    ~ElevationFinder();
-    /**
-     * Find an elevation.
-     * @returns Elevation in meters or -1 if no valid result was found.
-     * @args coordinates Coordinate of the point in wgs format, that is, unprojected.
-     */
-    int elevationWgs(const QPoint& coordinates);
-    /**
-     * Find an elevation.
-     * @returns Elevation in meters or -1 if no valid result was found.
-     * @args coordinates Coordinate of the point in map projection format.
-     */
-    int elevationMap(const QPoint& coordinates);
-    /**
-     * Find an elevation.
-     * @returns Elevation in meters or -1 if no valid result was found.
-     * @args WgsCoordinates Coordinate of the point in wgs format, that is, unprojected.
-     * @args MapCoordinates Coordinate of the point in map projection format.
-     * Both arguments should represent the same point. The format that results in the 
-     * fastest lookup for the method in use will be used. This is the preferred
-     * method of getting the elevation.
-     */
-    int elevation(const QPoint& WgsCoordinates, const QPoint& MapCoordinates);
-    /**
-     * @returns A pointer to the instance of the object to use. Use only this static
-     * method to get an ElevationFinder object!
-     */
-    static ElevationFinder * instance();
-    /**
-     * @returns true if KFLog we rely on the isohypses to find the elevation for a position.
-     */
-    bool useIsohypseForElevation() {return !useOGIE;};
-    
 private slots:
-    void timeout();
+
+  void timeout();
 
 private:
-    int findDEMelevation(const QPoint&);
-    bool tryOpenGLIGCexplorer();
-    
-    static ElevationFinder * _instance;
-    bool useOGIE;
-    
-    //name of DEM (Digital Elevation Model) file
-    QString demFileName;
-    //top left coordinate of Dem file 
-    QPoint demTL;
-    //bottom right coordinate of Dem file
-    QPoint demBR;
-    int demRows;
-    int demCols;
-    int demGridLat;
-    int demGridLon;
-    QTimer * timer;
-    QFile * demFile;
-    QDataStream * demStream;
+
+  int findDEMelevation(const QPoint&);
+  bool tryOpenGLIGCexplorer();
+
+  bool useOGIE;
+
+  //name of DEM (Digital Elevation Model) file
+  QString demFileName;
+  //top left coordinate of Dem file
+  QPoint demTL;
+  //bottom right coordinate of Dem file
+  QPoint demBR;
+  int demRows;
+  int demCols;
+  int demGridLat;
+  int demGridLon;
+  QTimer * timer;
+  QFile * demFile;
+  QDataStream * demStream;
 };
 
 #endif
