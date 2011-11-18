eb6425008ab7022e21317996d0540334f4119289
hendrik <hendrik@60db0586-fb42-0410-ab30-5566f8710cf8>
2011-11-18 22:51:01 +0000

    improve output of latlon2tile.py for copy'n'pastibility

--- geodata/latlon2tile.py
+++ geodata/latlon2tile.py
@@ -5,6 +5,6 @@ import sys
 LAT = int(sys.argv[1])
 LON = int(sys.argv[2])
 
-print "LAT =", LAT
-print "LON =", LON
-print "TILE =", 180*int((LAT-90)/-2) + int((LON+180)/2)
+print "LAT=%s"  %LAT
+print "LON=%s"  %LON
+print "TILE=%s" %(180*int((LAT-90)/-2) + int((LON+180)/2))
