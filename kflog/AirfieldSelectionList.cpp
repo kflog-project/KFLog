1638274a35e95ac3d9b411dca47ac0756f0553c8
Axel Pauli <kflog.cumulus@gmail.com>
2014-09-10 15:58:34 +0200

    new widget for selecting an airfield from a list

---
+++ kflog/AirfieldSelectionList.cpp
@@ -0,0 +1,151 @@
+/***********************************************************************
+**
+**   AirfieldSelectionList.cpp
+**
+**   This file is part of KFLog.
+**
+************************************************************************
+**
+**   Copyright (c):  2014 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+***********************************************************************/
+
+#include <QtGui>
+
+#include "AirfieldSelectionList.h"
+#include "mapcontents.h"
+
+extern MapContents *_globalMapContents;
+
+AirfieldSelectionList::AirfieldSelectionList( QWidget *parent ) :
+ QWidget(parent)
+{
+  setObjectName( "AirfieldSelectionList" );
+
+  m_searchEntry  = new QLineEdit;
+  connect( m_searchEntry, SIGNAL(returnPressed ()), SLOT(slotReturnPressed()) );
+  connect( m_searchEntry, SIGNAL(textEdited(const QString&)),
+           this, SLOT(slotTextEdited(const QString&)) );
+
+  m_selectionBox = new QComboBox;
+
+  QPushButton* clearButton = new QPushButton(tr("Clear"));
+  connect( clearButton, SIGNAL(clicked()),
+           SLOT(slotClearSearchEntry()));
+
+  QPushButton* setButton = new QPushButton(tr("Set"));
+  connect( setButton, SIGNAL(clicked()),
+           SLOT(slotSetSelectedEntry()));
+
+  QGridLayout* groupLayout = new QGridLayout;
+
+  m_groupBox = new QGroupBox;
+  m_groupBox->setLayout( groupLayout );
+
+  int row = 0;
+  groupLayout->addWidget( m_searchEntry, row, 0 );
+  groupLayout->addWidget( clearButton, row, 1 );
+  row++;
+  groupLayout->addWidget( m_selectionBox, row, 0 );
+  groupLayout->addWidget( setButton, row, 1 );
+  groupLayout->setColumnStretch( 0, 5 );
+
+  QHBoxLayout* hbox = new QHBoxLayout( this );
+  hbox->setSpacing(0);
+  hbox->addWidget( m_groupBox );
+}
+
+AirfieldSelectionList::~AirfieldSelectionList()
+{
+}
+
+void AirfieldSelectionList::showEvent( QShowEvent *event )
+{
+  fillSelectionBox();
+  QWidget::showEvent( event );
+}
+
+void AirfieldSelectionList::fillSelectionBox()
+{
+  int searchList[] = { MapContents::GliderfieldList, MapContents::AirfieldList };
+
+  QString selectedItemText = m_selectionBox->currentText();
+
+  m_airfieldDict.clear();
+  m_searchEntry->clear();
+  m_selectionBox->clear();
+
+  QStringList airfieldList;
+
+  for( int l = 0; l < 2; l++ )
+    {
+      for( int loop = 0; loop < _globalMapContents->getListLength(searchList[l]); loop++ )
+      {
+        SinglePoint *hitElement = (SinglePoint *) _globalMapContents->getElement(searchList[l], loop );
+        airfieldList.append( hitElement->getName() );
+        m_airfieldDict.insert( hitElement->getName(), hitElement );
+      }
+  }
+
+  airfieldList.sort();
+  m_selectionBox->addItems( airfieldList );
+
+  // try to find the last selection in the new content.
+  int newIndex = m_selectionBox->findText( selectedItemText );
+
+  if( newIndex != -1 )
+    {
+      // Try to find the last selection.
+      m_selectionBox->setCurrentIndex( newIndex );
+    }
+  else
+    {
+      m_selectionBox->setCurrentIndex( 0 );
+    }
+}
+
+void AirfieldSelectionList::slotClearSearchEntry()
+{
+  m_searchEntry->clear();
+  m_selectionBox->setCurrentIndex( 0 );
+}
+
+void AirfieldSelectionList::slotSetSelectedEntry()
+{
+  QString itemText = m_selectionBox->currentText();
+
+  if( itemText.isEmpty() )
+    {
+      return;
+    }
+
+  const SinglePoint *sp = 0;
+
+  if( m_airfieldDict.contains(itemText) )
+    {
+      sp = m_airfieldDict.value(itemText);
+
+      if( sp != 0 )
+	{
+	  emit takeThisPoint( sp );
+	}
+    }
+}
+
+void AirfieldSelectionList::slotReturnPressed()
+{
+  slotSetSelectedEntry();
+}
+
+void AirfieldSelectionList::slotTextEdited( const QString& text )
+{
+  int idx = m_selectionBox->findText( text, Qt::MatchStartsWith );
+
+ if( idx != -1 )
+    {
+      m_selectionBox->setCurrentIndex( idx );
+    }
+}
---
+++ kflog/AirfieldSelectionList.h
@@ -0,0 +1,122 @@
+/***********************************************************************
+**
+**   AirfieldSelectionList.h
+**
+**   This file is part of KFLog.
+**
+************************************************************************
+**
+**   Copyright (c):  2014 by Axel Pauli
+**
+**   This file is distributed under the terms of the General Public
+**   License. See the file COPYING for more information.
+**
+***********************************************************************/
+
+/**
+ * \class AirfieldSelectionList
+ *
+ * \author Axel Pauli
+ *
+ * \brief A widget with a combo box list and a search function for airfields.
+ *
+ * A widget with a combo list box and a search function for airfields. With the
+ * help of the search function you can navigate to a certain entry in the combo
+ * list box. The current selected entry in the combo box list is emitted as
+ * signal, if a set button is clicked.
+ *
+ * \date 2014
+ *
+ * \version 1.0
+ */
+
+#ifndef AirfieldSelectionList_h
+#define AirfieldSelectionList_h
+
+#include <QComboBox>
+#include <QGroupBox>
+#include <QHash>
+#include <QLineEdit>
+#include <QString>
+#include <QWidget>
+
+#include "singlepoint.h"
+
+class AirfieldSelectionList : public QWidget
+{
+  Q_OBJECT
+
+ private:
+
+  Q_DISABLE_COPY ( AirfieldSelectionList )
+
+ public:
+
+  AirfieldSelectionList( QWidget *parent=0 );
+
+  virtual ~AirfieldSelectionList();
+
+  /**
+   * Gets the selection box pointer.
+   *
+   * \return Pointer to selection box.
+   */
+  QComboBox* getSelectionBox()
+  {
+    return m_selectionBox;
+  }
+
+  /**
+   * Called to fill the selection box with content.
+   */
+  void fillSelectionBox();
+
+  void setGroupBoxTitle( QString title )
+  {
+    m_groupBox->setTitle( title );
+  };
+
+ protected:
+
+  void showEvent( QShowEvent *event );
+
+ signals:
+
+  /**
+   * Emitted, if the set button is pressed to broadcast the selected point.
+   */
+  void takeThisPoint( const SinglePoint* singePoint );
+
+ private slots:
+
+ /**
+  * Called, if the clear button is clicked.
+  */
+  void slotClearSearchEntry();
+
+  /**
+   * Called, if the set button is clicked to take over the selected entry from
+   * the combo box.
+   */
+  void slotSetSelectedEntry();
+
+  /**
+   * Called if the return key is pressed.
+   */
+  void slotReturnPressed();
+
+  /**
+   * Called if the text in the search box is modified.
+   */
+  void slotTextEdited( const QString& text );
+
+ private:
+
+  QGroupBox* m_groupBox;
+  QLineEdit* m_searchEntry;
+  QComboBox* m_selectionBox;
+
+  QHash<QString, SinglePoint*> m_airfieldDict;
+};
+
+#endif /* AirfieldSelectionList_h */
