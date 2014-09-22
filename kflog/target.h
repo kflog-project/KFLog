0afd0b6e4c30355c0add59e0e3f02d78a5c4a870
Axel Pauli <kflog.cumulus@gmail.com>
2014-09-22 09:05:12 +0200

    setting of next planned release

--- kflog/target.h
+++ kflog/target.h
@@ -8,24 +8,22 @@
 
     copyright            : (C) 2010-2014 by Axel Pauli
 
-    Email of maintainer  : hendrik@kflog.org
+    Email of maintainer  : kflog.cumulus@gmail.com
 
- ***************************************************************************/
+***************************************************************************/
 
 /***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   $Id$
- *                                                                         *
+*                                                                          *
+*    This program is free software; you can redistribute it and/or modify  *
+*    it under the terms of the GNU General Public License as published by  *
+*    the Free Software Foundation; either version 2 of the License, or     *
+*    (at your option) any later version.                                   *
+*                                                                          *
 ***************************************************************************/
 
 #ifndef target_h
 #define target_h
 
-#define KFLOG_VERSION "4.8.0"
+#define KFLOG_VERSION "4.9.0"
 
 #endif
